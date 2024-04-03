26e0ee25af845134d2610d00c340198868b819b9
nyh <nsi3358@naver.com>
2024-04-03 21:48:48 +0900

    FEAT : JWT 토근 발행
    로그인시 JWT 토큰 발행

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,12 +5,6 @@ const utils = {
     return crypto.createHash('sha512').update(pwd).digest('base64');
   },
 
-  randomPetNumber() {
-    const totalPets = 5;
-    const randomNumber = Math.floor(Math.random() * totalPets + 1);
-    return randomNumber;
-  },
-
   /**모든 값이 문자열이면 true를 반환 */
   isValidStringData(...values: unknown[]) {
     return values.every(value => typeof value === 'string');
